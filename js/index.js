400d6e0f2b3650e3cf8779b00f1c1550d088b91a
GMartinez0210 <genaro010204@gmail.com>
2022-05-07 21:34:39 -0500

    Missing the responsive design

--- js/index.js
+++ js/index.js
@@ -1,45 +1,45 @@
     //  Images
 //  Thumbnail
-const thumbnail_imgs = ["image-product-1-thumbnail.jpg", "image-product-2-thumbnail.jpg", "image-product-3-thumbnail.jpg", "image-product-4-thumbnail.jpg"]
+const thumbnail_imgs = [
+    "./images/image-product-1-thumbnail.jpg", 
+    "./images/image-product-2-thumbnail.jpg", 
+    "./images/image-product-3-thumbnail.jpg", 
+    "./images/image-product-4-thumbnail.jpg"
+]
 // No Thumbnail
-const main_imgs = ["image-product-1.jpg", "image-product-2.jpg", "image-product-3.jpg", "image-product-4.jpg"]
-
-//  Trash
-const icon_delete = "icon-delete.svg"
+const main_imgs = [
+    "./images/image-product-1.jpg", 
+    "./images/image-product-2.jpg", 
+    "./images/image-product-3.jpg", 
+    "./images/image-product-4.jpg"
+]
 
     //  Document variables
-const cart = $('.cart')
+// the big image shown, the main one
 const main_product = $('.main-product')
-const more_products = $('.more-products')
-const price = $('.price') 
-const minus_img = $('.minus-img')
-const plus_img = $('.plus-img')
+
+// the quantity from the minus plus action
 const quantity = $('.quantity')
+// the container which has the view of the cart
 const view_cart = $('.view-cart')
-const empty_cart = $('.empty')
-const noempty_cart = $('.no-empty')
-const adding = $('.adding')
+// the message we show when the cart is empty
+const empty_cart = $('.empty-cart')
+// the table which has the information of the item
+const cart_table = $('.cart-table')
+// the quantity number of the cart
+const cart_quantity = $('.cart-quantity')
+const quantity_cart = $('.quantity-cart')
+// total to pay 
+const total = $('.total')
 
-minus_img.click(() => {
-    if (Number(quantity.text()) > 0) {
-       quantity.text((index, text) => {
-           return Number(text) - 1 
-       })
-    }
-})
 
-plus_img.click(() => {
-    quantity.text((index, text) => {
-        return Number(text) + 1 
-    })
-})
 
-cart.click(() => {
-    view_cart.toggleClass('show-cart')
-})
+// Changing the main image clicking the thumb ones
+// the div where the thumbnail images are
+const more_products = $('.more-products')
 more_products.children().map((index, image) => {
     image.addEventListener("click", () => {
-        main_product.attr("src", `./images/${main_imgs[index]}`)
+        main_product.attr("src", `${main_imgs[index]}`)
         
         more_products.children().map((id, img) => {
             img.classList.remove("shown-product")
@@ -49,127 +49,140 @@ more_products.children().map((index, image) => {
     })
 })
 
+// To change the quantity number
+// the minus and plus symbol
+// To minus one by one 
+const minus_img = $('.minus-img').click(() => {
+    if (Number(quantity.text()) != 0) {
+       quantity.text((index, content) => {
+           return Number(content) - 1 
+       })
+    }
+})
 
-
-/* 
-    //  Changing the quantity
-let quantity_variable = 0
-quantity.innerHTML = quantity_variable
-
-
-
-    //  Changing the main product image
-for(let i = 0; i < more_products.children.length; i++){
-    more_products.children[i].addEventListener('click', () => {
-        for(let j = 0; j < more_products.children.length; j++) {
-            more_products.children[j].classList.remove('shown-product')
-        }
-        if(!more_products.children[i].classList.contains('shown-product')) {
-            more_products.children[i].classList.add('shown-product')
-            main_product.setAttribute('src', './images/' + main_imgs[i])
-        }
+// To add one by one
+const plus_img = $('.plus-img').click(() => {
+    quantity.text((index, content) => {
+        return Number(content) + 1 
     })
-}
-
-const cart_container = document.createElement('div')
-view_cart.appendChild(cart_container)
-cart_container.classList.add('cart-container')
-
-let bougth_unids = 0
-
-    //  Viewing the cart
-function Viewing_Cart () {
-    view_cart.classList.toggle('show-cart')
-
-    if(bougth_unids == 0 & cart_container.children.length == 0) {
-        let empty_cart = document.createElement('p')
-        cart_container.appendChild(empty_cart)
-        empty_cart.innerHTML = "Your cart is empty"
-        empty_cart.classList.add('empty-cart')
-    }
-    else if (cart_container.children[0].classList.contains('empty-cart') & cart_container.children.length == 1) {
-        let empty_cart = document.querySelector('.empty-cart')
-        cart_container.removeChild(empty_cart)
-    }
-}
-
-cart.addEventListener('click', () => {
-    Viewing_Cart()
 })
 
-    //  Adding to the cart
-let num_products = 1
-
-let table = document.createElement('table')
-let toPay = 0
-
-adding.addEventListener('click', () => {
-    
-    bougth_unids += quantity_variable
-    toPay = bougth_unids * 125.00
-
-    if (quantity_variable != 0) {
-        try {
-            if (cart_container.children[0].classList.contains('empty-cart') & cart_container.children.length == 1) {
-                let empty_cart = document.querySelector('.empty-cart')
-                cart_container.removeChild(empty_cart)
-            }
-        } catch (error) {}
-
-        if (cart_container.children.length == 0) {
-            view_cart.style.width = 'auto'
-
-            table.classList.add('cart-table')
-            cart_container.appendChild(table)
-
-            for(let i = 0; i < num_products; i++) {
-                let tr = document.createElement('tr')
-                table.appendChild(tr)
-                for(let j = 0; j < 3; j++) {
-                    let td = document.createElement('td')
-                    table.children[i].appendChild(td)
-                }
-                const cart_product = document.createElement('img')
-                table.children[i].children[0].appendChild(cart_product)
-                table.children[i].children[0].firstChild.setAttribute('src', "./images/" + thumbnail_imgs[0])
-                table.children[i].children[0].firstChild.classList.add('cart-product')
-
-                let cart_info = document.createElement('table')
-                table.children[i].children[1].appendChild(cart_info)
-                table.children[i].children[1].firstChild.classList.add('cart-info')
-
-                for(let j = 0; j < 2; j++) {
-                    let tr = document.createElement('tr')
-                    table.children[i].children[1].firstChild.appendChild(tr)
-                }
-
-                for(let j = 0; j < 2; j++) {
-                    let td = document.createElement('td')
-                    table.children[i].children[1].firstChild.children[j].appendChild(td)
-
-                    let p = document.createElement('p')
-                    table.children[i].children[1].firstChild.children[j].firstChild.appendChild(p)
-                }
-
-                table.children[i].children[1].firstChild.children[0].firstChild.firstChild.innerHTML = "Fall Limited Edition Sneakers"
-                
-                for(let j = 0; j < 2; j++) {
-                    let span = document.createElement('span')
-                    table.children[i].children[1].firstChild.children[1].firstChild.firstChild.appendChild(span)
-                }
-                table.children[i].children[1].firstChild.children[1].firstChild.firstChild.children[0].innerHTML = "$125.00 * " + bougth_unids
-                table.children[i].children[1].firstChild.children[1].firstChild.firstChild.children[1].innerHTML = "$" + toPay + ".00"
-
-                const cart_trash = document.createElement('img')
-                table.children[i].children[2].appendChild(cart_trash)
-                table.children[i].children[2].firstChild.setAttribute('src', "./images/" + icon_delete)
-                table.children[i].children[2].firstChild.classList.add('cart-trash')
-            }
-        }
-        else{
-            table.children[num_products - 1].children[1].firstChild.children[1].firstChild.firstChild.children[0].innerHTML = "$125.00 * " + bougth_unids
-            table.children[num_products - 1].children[1].firstChild.children[1].firstChild.firstChild.children[1].innerHTML = "$" + toPay + ".00"
-        }
+// To display the nav content
+// cart img variable
+const cart = $('.cart').click(() => {
+    view_cart.toggleClass('show-cart')
+    if (Number(quantity_cart.text()) > 0) {
+        cart_table.removeClass("empty")
+        empty_cart.addClass("empty")
+    }
+    else {
+        cart_table.addClass("empty")
+        empty_cart.removeClass("empty")
     }
 })
- */
+
+// the button to add items to the cart
+const adding = $('.adding').click(() => {
+    quantity_cart.text((index, content) => {
+        let number = Number(content) + Number(quantity.text())
+        if(number > 0) {
+            cart_table.removeClass("empty")
+            empty_cart.addClass("empty")
+            cart_quantity.removeClass("empty")
+        }
+        cart_quantity.text((id, text) => {
+            return number
+        })
+        total.text((id, text) => {
+            let total = Number(cart_quantity.text()) * 125
+            return `$ ${total}`
+        })
+        return number
+    })
+
+})
+
+// the trash of the cart to delete items, one by one
+const cart_trash = $('.cart-trash').click(() => {
+    if(Number(quantity_cart.text()) > 0) {
+        quantity_cart.text((index, content) => {
+            let number =  Number(content) - 1
+            if(number == 0) {
+                cart_table.addClass("empty")
+                empty_cart.removeClass("empty")
+                cart_quantity.addClass("empty")
+            }
+            cart_quantity.text((id, text) => {
+                return number
+            })
+            total.text((id, text) => {
+                let total = Number(cart_quantity.text()) * 125
+                return `$ ${total}`
+            })
+            return number
+        })
+    }
+})
+
+/* light Box */
+const more_products_light_box = $(".more-products-light-box")
+const light_box_img = $(".light-box-img")
+more_products_light_box.children().map((index, image) => {
+    image.addEventListener("click", () => {
+        light_box_img.attr("src", `${main_imgs[index]}`)
+        
+        more_products_light_box.children().map((id, img) => {
+            img.classList.remove("shown-product")
+        })
+        
+        image.classList.add("shown-product")
+    })
+})
+
+// Previous img of light box
+const light_box_previous = $(".light-box-previous").click(() => {
+    const long = main_imgs.length - 1
+    let index = main_imgs.indexOf(light_box_img.attr("src"))
+    if (index == 0) {
+        light_box_img.attr("src", `${main_imgs[long]}`)
+    }
+    else {
+        light_box_img.attr("src", `${main_imgs[index - 1]}`)
+    }
+
+    more_products_light_box.children().map((id, img) => {
+        img.classList.remove("shown-product")
+        if (id == index - 1) img.classList.add("shown-product")
+        else if (index == 0) more_products_light_box.children()[long].classList.add("shown-product")
+    })
+})
+
+// Next img of light box
+const light_box_next = $(".light-box-next").click(() => {
+    const long = main_imgs.length - 1
+    let index = main_imgs.indexOf(light_box_img.attr("src"))
+    if (index == long) {
+        light_box_img.attr("src", `${main_imgs[0]}`)
+    }
+    else {
+        light_box_img.attr("src", `${main_imgs[index + 1]}`)
+    }
+
+    more_products_light_box.children().map((id, img) => {
+        img.classList.remove("shown-product")
+        if (id == index + 1) img.classList.add("shown-product")
+        else if (index == 3) more_products_light_box.children()[0].classList.add("shown-product")
+    })
+})
+
+// Opeing the light box
+const light_box_open = $(".light-box-open").click(() => {
+    if (window.screen.width > 576) {
+        $(".light-box").css({"left": "0", "filter": "opacity(1)"})
+    }
+})
+
+// Closing the light box
+const light_box_close = $(".light-box-close").click(() => {
+    $(".light-box").css({"left": "-100%", "filter": "opacity(0)"})
+})
